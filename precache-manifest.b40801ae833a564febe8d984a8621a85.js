e52c758e8d8ee329b169dfaff2fb5f126b81f310
ArtyomRyabov <ArtyomRyabov@users.noreply.github.com>
2019-07-30 21:27:24 +0300

    Updates

---
+++ precache-manifest.b40801ae833a564febe8d984a8621a85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2018677aba20e9adfe7b1cf1a57eabb",
+    "url": "artyomryabov.github.io/index.html"
+  },
+  {
+    "revision": "65027adfe27e965134a0",
+    "url": "artyomryabov.github.io/static/css/main.b3cc5094.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "artyomryabov.github.io/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "65027adfe27e965134a0",
+    "url": "artyomryabov.github.io/static/js/main.f408f43b.chunk.js"
+  },
+  {
+    "revision": "954f9b27de327c5a7e2e",
+    "url": "artyomryabov.github.io/static/js/runtime~main.db63bc64.js"
+  }
+]);
